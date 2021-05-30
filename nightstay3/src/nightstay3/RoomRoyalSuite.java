9b467dc40d917cae96f9ebb55fc3c5ee495e6eac
Turgut Reiss <Turgut Reiss@DESKTOP-N1GBL64>
2021-05-31 00:58:31 +0800

    second commit: room classes updated 3 MAJOR UPDATE

---
+++ nightstay3/src/nightstay3/RoomRoyalSuite.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nightstay3;
+
+/**
+ *
+ * @author Turgut Reiss
+ */
+public class RoomRoyalSuite extends Roomtemplate{
+    double harga = 30.0;
+    double total = 0;
+    private int hari;
+    private String roomid;
+    private boolean status = false;
+    
+    public RoomRoyalSuite(String id){
+        roomid = id;
+    }
+    
+    @Override
+    public void order(int h){
+        hari = h;
+        total = harga * hari;
+        status = true;
+    }
+
+    
+    @Override
+    public double getharga(){
+        return harga;
+    }
+    
+    @Override
+    public double getTotal(){
+        return total;
+    }
+    
+    @Override
+    public String displaydescription(){
+        String res;
+        if (status != true){
+            res = ("Status: Available");
+        } else {
+            res = ("Status: Booked");
+        }
+        return ("Nomor Kamar: " + roomid
+                + "\no Tipe Kamar: Royal Suite"
+                + "\no Pricing: " + harga + " /malam"
+                + "\n" + res);
+    }
+}
