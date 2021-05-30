d0369c7ef9c31c22f3ec487a59068d8d38600a6b
Turgut Reiss <Turgut Reiss@DESKTOP-N1GBL64>
2021-05-30 19:09:38 +0800

    first commit

---
+++ nightstay3/src/nightstay3/Nightstay3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nightstay3;
+
+/**
+ *
+ * @author Turgut Reiss
+ */
+public class Nightstay3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ nightstay3/src/nightstay3/roomRoyalSuite.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nightstay3;
+
+/**
+ *
+ * @author Turgut Reiss
+ */
+public class roomRoyalSuite implements roomtemplate{
+    
+}
