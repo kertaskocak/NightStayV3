35a2ca8cdc06a45c31460c100968acb9d25acd06
Turgut Reiss <Turgut Reiss@DESKTOP-N1GBL64>
2021-05-30 21:03:15 +0800

    second commit: room classes updated 2 MAJOR UPDATE

--- nightstay3/src/nightstay3/roomJuniorSuite.java
+++ nightstay3/src/nightstay3/roomJuniorSuite.java
@@ -10,5 +10,43 @@ package nightstay3;
  * @author Turgut Reiss
  */
 public class roomJuniorSuite implements roomtemplate{
+    private double harga = 15.0;
+    private double total = 0;
+    private int hari;
+    private String roomid;
+    private boolean status = false;
     
+    public roomJuniorSuite(String id){
+        roomid = id;
+    }
+    
+    @Override
+    public void order(int h){
+        hari = h;
+        total = harga * hari;
+        status = true;
+    }
+
+    
+    @Override
+    public double getharga(){
+        return harga;
+    }
+    
+    @Override
+    public double getTotal(){
+        return total;
+    }
+    
+    @Override
+    public void displaydescription(){
+        System.out.println("Nomor Kamar: " + roomid);
+        System.out.println("Tipe Kamar: Junior Suite");
+        System.out.println("Pricing: " + harga + " /malam");
+        if (status != true){
+            System.out.println("Status: Available");
+        } else {
+            System.out.println("Status: Booked");
+        }
+    }
 }
--- nightstay3/src/nightstay3/roomRoyalSuite.java
+++ nightstay3/src/nightstay3/roomRoyalSuite.java
@@ -10,5 +10,43 @@ package nightstay3;
  * @author Turgut Reiss
  */
 public class roomRoyalSuite implements roomtemplate{
+    double harga = 30.0;
+    double total = 0;
+    private int hari;
+    private String roomid;
+    private boolean status = false;
     
+    public roomRoyalSuite(String id){
+        roomid = id;
+    }
+    
+    @Override
+    public void order(int h){
+        hari = h;
+        total = harga * hari;
+        status = true;
+    }
+
+    
+    @Override
+    public double getharga(){
+        return harga;
+    }
+    
+    @Override
+    public double getTotal(){
+        return total;
+    }
+    
+    @Override
+    public void displaydescription(){
+        System.out.println("Nomor Kamar: " + roomid);
+        System.out.println("Tipe Kamar: Royal Suite");
+        System.out.println("Pricing: " + harga + " /malam");
+        if (status != true){
+            System.out.println("Status: Available");
+        } else {
+            System.out.println("Status: Booked");
+        }
+    }
 }
